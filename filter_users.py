b553fe63d1fea4bc898fc66806bdc91c61ed9c3a
Rana Alnatsheh <ranaalnatsheh@Ranas-MacBook-Air.local>
2025-12-07 20:49:39 +0100

    Refactor code to include docstrings and PEP 8 formatting

--- filter_users.py
+++ filter_users.py
@@ -2,25 +2,34 @@ import json
 
 
 def filter_users_by_name(name):
+    """Filter and print users matching the given name."""
     with open("users.json", "r") as file:
         users = json.load(file)
+
     filtered_users = [user for user in users if user["name"].lower() == name.lower()]
+
     for user in filtered_users:
         print(user)
 
 
 def filter_users_by_age(age):
+    """Filter and print users matching the given age."""
     with open("users.json", "r") as file:
         users = json.load(file)
+
     filtered_users = [user for user in users if user["age"] == int(age)]
+
     for user in filtered_users:
         print(user)
 
 
 def filter_users_by_email(email):
+    """Filter and print users matching the given email."""
     with open("users.json", "r") as file:
         users = json.load(file)
+
     filtered_users = [user for user in users if user["email"].lower() == email.lower()]
+
     for user in filtered_users:
         print(user)
 
